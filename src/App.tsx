1cbb2afd166d20b8f79e08d76b73e1f1ba9b89b7
DonSuulio <nsewell0210@gmail.com>
2025-11-14 23:01:41 -0600

    Create App component with initial layout

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh',
+      backgroundColor: '#1a2332',
+      color: '#c0c5ce',
+      fontFamily: 'Merriweather, serif'
+    }}>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Ansero E-Discovery Platform</h1>
+        <p>Builder.io integration ready</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
